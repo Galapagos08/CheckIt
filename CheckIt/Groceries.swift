e5c88ce2e78ae8494c7c89818cd8b0e53343902c
Dan Esrey <danesrey@dans-macbook-air.local>
2016-12-07 10:20:18 -0700

    added Groceries

---
+++ CheckIt/Groceries.swift
@@ -0,0 +1,18 @@
+//
+//  Groceries.swift
+//  CheckIt
+//
+//  Created by Dan Esrey on 2016/05/12.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+import Foundation
+
+class GroceryList {
+    
+    let dairyList = ["Milk", "Eggs", "Cheese", "Butter", "Yogurt", "Sour Cream", "Half & Half", "Cottage Cheese"]
+    let vegList = ["Asparagus", "Broccoli", "Cabbage", "Carrots", "Cauliflower", "Celery", "Corn", "Cucumbers", "Lettuce", "Mushrooms", "Onions", "Peppers", "Potatoes", "Spinach", "Squash", "Tomatoes", "Zucchini"]
+    let fruitList = ["Apples", "Avocados", "Bananas", "Blueberries", "Cherries", "Grapefruit", "Grapes"]
+}
+
+// let groceryItems = ["Dairy": dairyList, "Vegetables": vegList]
